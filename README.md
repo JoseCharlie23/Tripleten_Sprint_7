b4c6b2029523c4f8e5854d248419513219e0bd59
alcachoque <josecarloscompe@gmail.com>
2025-10-21 21:11:19 -0500

    Upgraded app.py // Added EDA.ipynb notebook // README now includes a description of the app.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Tripleten_Sprint_7
 Sprint 7 Project by Jose Alca.
+Esta aplicación hecha con Streamlit es una app web interactiva de visualización de datos que permite explorar un conjunto de datos sobre anuncios de autos usados (vehicles_us.csv).
+1) Carga el archivo CSV.
+    - Precio (price).
+    - Kilometraje (odometer)
+    - Año, marca, modelo, etc.
+2) Muestra controles interactivos (casillas de verificacion).
+3) Genera graficos dinamicos.
+    - Si el usuario marca la casilla, el app muestra como se distribuyen los valores de kilometraje (odometro)
+    - Si marca la casilla del grafico de dispersion, la app muestra la relacion del kilometraje y el precio de auto.
---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+# Leer los datos
+car_data = pd.read_csv(
+    'C:/Users/Usuario/Proyectos/Tripleten_Sprint_7/vehicles_us.csv')
+# Crear casillas de verificacion.
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+
+# Si el usuario marca la casilla de histograma
+if build_histogram:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Si el usuario marca la casilla de dispersión
+if build_scatter:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price",
+                     title="Relación entre odómetro y precio")
+    st.plotly_chart(fig, use_container_width=True)
